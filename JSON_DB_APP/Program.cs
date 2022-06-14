1496f7bf5bb7b1b8b9036f534c1ad604645fece8
vvwind <vladimirwind@yahoo.com>
2022-06-14 19:35:15 +0300

    Release

--- JSON_DB_APP/Program.cs
+++ JSON_DB_APP/Program.cs
@@ -14,23 +14,57 @@ namespace JSON_DB_APP
 
     class Program
     {
-        private static string fileName = "all_heroes_sets.json";
-        private static string jsonString = File.ReadAllText(fileName);
+        public static string fileName = "all_heroes_sets.json";
+        public static string jsonString = File.ReadAllText(fileName);
+        
+        
 
-        private static Dictionary<string, object> result = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
 
-
-        private static string connectionString = ConfigurationManager.ConnectionStrings["JSON_DB"].ConnectionString;
-        private static SqlConnection sqlConnection = null;
+        public static Dictionary<string, string> new_result;
+        public static Dictionary<string, object> result = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
+        public static string a;
+        public static string chor = "\"";
+        public static string connectionString = ConfigurationManager.ConnectionStrings["JSON_DB"].ConnectionString;
+        public static SqlConnection sqlConnection = null;
+        
         static void Main(string[] args)
         {
+        
             Console.WriteLine("JSON DB");
-           
+            foreach (var kvp in result)
+            {
+                a = kvp.Value.ToString();
+                if (a.Length > 127)
+                {
+                    string aa = kvp.Value.ToString().Substring(0, 127);
+                    string ab = aa.Replace('\"', ' ');
+                    string ab1 = ab.Replace('\'', '_');
+                    string ab2 = ab1.Replace('{', '\"');
+                    string ab3 = ab2.Replace('}', '\"');
+                   
+
+                    result[kvp.Key] = ab3;
+                }
+                else 
+                {
+                    string aa = a;
+                    string ab = aa.Replace('\"', ' ');
+                    string ab1 = ab.Replace('`', '_');
+                    string ab2 = ab1.Replace('{', ' ');
+                    string ab3 = ab2.Replace('}', ' ');
+                   
+
+                    result[kvp.Key] = ab3;
+                }
+               
+                
+            }
+            
             sqlConnection = new SqlConnection(connectionString);
             sqlConnection.Open();
            
             SqlDataReader sqlDataReader = null;
-
+           
             string command = string.Empty;
 
             while (true)
@@ -59,27 +93,35 @@ namespace JSON_DB_APP
                     }
 
 
-                   
+
                     if (command.ToLower().Equals("json")) //CONSTRUCTOR
                     {
+
                         foreach (KeyValuePair<string, object> kvp in result)
 
                         {
+
+
                             int leng = kvp.Value.ToString().Length;
-                            if (leng > 128)
+                            if (leng > 127)
                             {
-                                string query = $"INSERT INTO hero_set_ (hero,sets) VALUES ({kvp.Key},{kvp.Value.ToString().Substring(0, 128) })";
+                                string cos = kvp.Value.ToString();
+                                
+                                string query = $"INSERT INTO hero_set_ (hero,sets) VALUES ({kvp.Key.ToString()},{cos})";
                                 SqlCommand command_json = new SqlCommand(query, sqlConnection);
-                          
+                                Console.WriteLine($"Added {command_json.ExecuteNonQuery()} row(s)");
                             }
                             else
                             {
-                                string query = $"INSERT INTO hero_set_ (hero,sets) VALUES ({kvp.Key},{kvp.Value.ToString().Substring(0, leng) })";
-                                SqlCommand command_json = new SqlCommand(query, sqlConnection);
-                              
+                                string query1 = $"INSERT INTO hero_set_ (hero,sets) VALUES ('{kvp.Key.ToString()}','{kvp.Value.ToString().Substring(0, leng) }')";
+                                SqlCommand command_json = new SqlCommand(query1, sqlConnection);
+                                Console.WriteLine($"Added {command_json.ExecuteNonQuery()} row(s)");
                             }
+
+
+
                         }
-                        
+                        break;
                     }
 
 
